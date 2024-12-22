8acd27c85676c71803e4bc03632e9f6bef97fc38
Atharva <athrudev@gmail.com>
2024-12-22 15:36:21 +0530

    successfully solved the half pyramid problem

---
+++ patterns/halfpyramid.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class halfpyramid{
+    public static void main(String args[]){
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter no lines: ");
+
+        int n=sc.nextInt();
+
+        for(int i=1;i<=n;i++){ // outer loop to loop entire lines
+            for(int j=1;j<=i;j++){ // this loop to print each line
+                System.out.print("*");
+               /* 5 System.out.print("i: "+i);
+                System.out.print("j: "+j);*/
+
+            }
+            System.out.println();
+        }
+
+
+    }
+
+}
