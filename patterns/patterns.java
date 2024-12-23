edcc3dbcfd05bc4e0aa2e8595700c446ec0ad93c
Atharva <athrudev@gmail.com>
2024-12-23 10:46:59 +0530

    solved the star pattern problem

--- patterns/patterns.java
+++ patterns/patterns.java
@@ -1,7 +1,20 @@
 
-
 public class patterns {
+
+        public static void star_pattern(int n){
+
+            for(int i=1;i<=n;i++){ // to traverse lines
+                for(int j=1;j<=i;j++){//to print each line
+                    System.out.print("*");
+                }
+                System.out.println("");
+            }
+
+        }
+
+
     public static void main(String[] args) {
-        
+        int n=5;
+        star_pattern(n);
     }
 }
