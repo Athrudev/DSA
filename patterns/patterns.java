9acad04214da7f345cadd7905c7bbad12dabd3cb
Atharva <athrudev@gmail.com>
2024-12-23 10:50:58 +0530

    solved the inverted star pattern

--- patterns/patterns.java
+++ patterns/patterns.java
@@ -13,8 +13,21 @@ public class patterns {
         }
 
 
+        public static void inverted_star_patten(int n){
+            for(int i=n;i>=1;i--){
+                for(int j=1;j<=i;j++){
+                    System.out.print("*");
+                }
+                System.out.println("");
+            }
+        }
+
+
     public static void main(String[] args) {
         int n=5;
+        System.out.println("\nStar pattern\n");
         star_pattern(n);
+        System.out.println("\nInverted Star pattern\n");
+        inverted_star_patten(n);
     }
 }
