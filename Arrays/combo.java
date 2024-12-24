c914fe974dcf44d8cfe947444be795a4584bd1f1
Atharva <athrudev@gmail.com>
2024-12-24 16:30:23 +0530

    solved the print pairs, and subarrays problem

--- Arrays/combo.java
+++ Arrays/combo.java
@@ -35,11 +35,39 @@ public class combo {
         
     }
 
+
+
+    public static void print_pairs(int a[]){
+        int n=a.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                System.out.print("("+a[i]+","+a[j]+")");
+            }
+            System.out.println("");
+        }
+    }
+
+
+    public static void print_sub_arrays(int a[]){
+        int n=a.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                for(int k=i;k<=j;k++){
+                    System.out.print(k+" ");
+                }
+                System.out.println("");
+            }
+            System.out.println("");
+        }
+    }
+
     public static void main(String[] args) {
 
         int a[]={2,4,6,8,10};
 
-        reverse_array(a);
+        //reverse_array(a);
+        //print_pairs(a);
+        print_sub_arrays(a);
 
         
     }
