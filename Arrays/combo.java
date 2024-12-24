54f6d8dc0982f96dc1d776f3d96f6694129f4303
Atharva <athrudev@gmail.com>
2024-12-24 16:41:33 +0530

    solved the max sum ssubarray with brute force appraoch

--- Arrays/combo.java
+++ Arrays/combo.java
@@ -61,13 +61,48 @@ public class combo {
         }
     }
 
+    public static void brute_force_approach_max_subarray(int a[]){
+        int n=a.length;
+        int cs=0;
+        int ms=Integer.MIN_VALUE;
+
+        for(int i=0;i<n;i++){//for each number
+            for(int j=i+1;j<n;j++){//for sub array of each number
+                cs=0;
+                for(int k=i;k<=j;k++){
+                    cs+=a[k];
+                }
+                ms=Math.max(ms, cs);
+            }
+            
+        }
+
+
+        System.out.println("Maximum sum of an sub array is:"+ms);
+    }
+
+
+
+    public static void prefix_approach_sum(int a[]){
+        int n=a.length;
+        int cs=0;
+        int ms=Integer.MIN_VALUE;
+        
+
+    }
+
+
+
     public static void main(String[] args) {
 
         int a[]={2,4,6,8,10};
+        //int a2[]={};
 
         //reverse_array(a);
         //print_pairs(a);
-        print_sub_arrays(a);
+        //print_sub_arrays(a);
+        //brute_force_approach_max_subarray(a);
+        prefix_approach_sum(a);
 
         
     }
