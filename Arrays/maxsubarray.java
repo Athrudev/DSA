fe943dd2739ad0b3c98ab900020207644750ef31
Atharva <athrudev@gmail.com>
2024-12-24 11:37:19 +0530

    solved the maximum sub array problem

---
+++ Arrays/maxsubarray.java
@@ -0,0 +1,33 @@
+package Arrays;
+import java.util.*;
+
+public class maxsubarray {
+
+
+    public static void print_max_subarray(int a[]){
+        int n=a.length;
+        int cs=0;
+        int max=Integer.MIN_VALUE;
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                cs=0;
+                for(int k=i;k<=j;k++){
+                    cs+=a[k];
+                }
+                if(cs>max){
+                    max=cs;
+                }
+                System.out.println(cs);
+            }
+            
+        }
+        System.out.println(max);
+    }
+
+
+    public static void main(String args[]){
+        int a[]={2,4,6,8,10};
+        print_max_subarray(a);
+    }
+}
