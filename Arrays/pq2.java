8b4b6d81ac319d5262f12f690344c8f9a9852db4
Atharva <athrudev@gmail.com>
2024-12-26 12:00:55 +0530

    solved the practice question 2

---
+++ Arrays/pq2.java
@@ -0,0 +1,44 @@
+package Arrays;
+import java.util.*;
+
+public class pq2 {
+
+    public static int solution(int nums[],int target){
+        int n=nums.length;
+        int min=0;
+
+        for(int i=1;i<n;i++){
+            int current=i;
+            if(nums[current]<nums[min]){
+                min=current;
+            }
+        }
+
+        if(nums[min]==target){
+            return min;
+        }else{
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        //test case 1
+        int nums1[]={4,5,6,7,0,1,2};
+        int target1=0;
+
+        //test case 2
+        int nums2[]={4,5,6,7,0,1,2};
+        int target2=3;
+
+        //test case 3
+        int nums3[]={1};
+        int target3=0;
+
+
+        System.out.println("test case 1:"+solution(nums1,target1));
+        System.out.println("test case 2:"+solution(nums2,target2));
+        System.out.println("test case 3:"+solution(nums3,target3));
+        
+    }
+}
--- Arrays/practiceq.java
+++ Arrays/practiceq.java
@@ -1,9 +1,9 @@
 package Arrays;
 
-public class practiceq1 {
+public class practiceq {
 
 
-    public static boolean solution(int nums[]){
+    public static boolean Question1(int nums[]){
         
         int n=nums.length;
         boolean found=false;
@@ -21,13 +21,20 @@ public class practiceq1 {
         return found;
     }
 
+
+
+    
      public static void main(String[] args) {
         int nums1[]={1,2,3,1};
         int nums2[]={1,2,3,4};
         int nums3[]={1,1,1,3,3,4,3,2,4,2};
-        System.out.println("Test 1: "+solution(nums1)); //true
-        System.out.println("Test 2: "+solution(nums2)); //false
-        System.out.println("Test 3: "+solution(nums3)); //true
+        System.out.println("Test 1: "+Question1(nums1)); //true
+        System.out.println("Test 2: "+Question1(nums2)); //false
+        System.out.println("Test 3: "+Question1(nums3)); //true
+
+
+
+
         
      }
 }
