30d95ddccf62d371bf795f60baf37a3209ff70d3
Atharva <athrudev@gmail.com>
2024-12-26 11:31:58 +0530

    solved the first practice question by myself

---
+++ Arrays/practiceq1.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+public class practiceq1 {
+
+
+    public static boolean solution(int nums[]){
+        
+        int n=nums.length;
+        boolean found=false;
+
+        for(int i=0;i<n;i++){
+            int current=nums[i];
+            for(int j=i+1;j<n;j++){
+                if(current==nums[j]){
+                    found=true;
+                    break;
+                }
+            }
+        }
+        
+        return found;
+    }
+
+     public static void main(String[] args) {
+        int nums1[]={1,2,3,1};
+        int nums2[]={1,2,3,4};
+        int nums3[]={1,1,1,3,3,4,3,2,4,2};
+        System.out.println("Test 1: "+solution(nums1)); //true
+        System.out.println("Test 2: "+solution(nums2)); //false
+        System.out.println("Test 3: "+solution(nums3)); //true
+        
+     }
+}
