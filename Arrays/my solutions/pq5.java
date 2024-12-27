3dbfbb884c623a13eb198ebf1d999e8c16198e75
Atharva <athrudev@gmail.com>
2024-12-27 10:32:49 +0530

    solved the solution using mam's approach

--- Arrays/my solutions/pq5.java
+++ Arrays/my solutions/pq5.java
@@ -7,8 +7,12 @@ Notice that the solution set must not contain duplicate triplets.
 
  */
 
+import java.util.*;;
+
 public class pq5 {
 
+
+    //i failed to solve this problem in my way
 
     public static void solution(int nums[]){
         int n=nums.length;
@@ -29,10 +33,46 @@ public class pq5 {
 
     }
 
+
+
+    public static List<List<Integer>> threeSum(int nums []) {
+
+            List<List<Integer>> result = new ArrayList <List<Integer>> ();
+
+            for(int i=0; i<nums.length; i++) {
+
+                for(int j=i+1; j<nums.length; j++) {
+
+                    for(int k=j+1; k<nums. length; k++) {
+
+                        if (nums [i] + nums [j] + nums [k] == 0) {
+
+                            List<Integer> triplet = new ArrayList < Integer > () ;
+
+                            triplet.add (nums [i]) ;
+
+                            triplet.add (nums [j]) ;
+
+                            triplet.add(nums [k]);
+
+                            Collections.sort (triplet) ;
+
+                            result.add(triplet);
+                        }
+                    }
+            }
+        }
+                
+
+            result = new ArrayList<List<Integer>> (new LinkedHashSet<List<Integer>> (result));
+
+            return result;
+    }
+
     public static void main(String[] args) {
         int nums[]={-1,-0,1,2,-1,-4};
-        solution(nums); 
-        //System.out.println("test case 1: "+solution(nums));
+        //solution(nums); 
+        System.out.println("test case 1: "+threeSum(nums));
         
     }
 }
