6193f14b985a4172a633533937d6d6494eb40ea5
Atharva <athrudev@gmail.com>
2024-12-27 11:21:17 +0530

    implimented the selection sort

--- sort/basicSort.java
+++ sort/basicSort.java
@@ -8,6 +8,17 @@ public class basicSort {
         a[j]=temp;
     }
 
+    public static void printArr(int a[]){
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+" ");
+            
+        }
+        System.out.println("");
+    }
+
+
+
+
     public static void bubbleSort(int a[]){
         int n=a.length;
 
@@ -27,21 +38,36 @@ public class basicSort {
 
     }
 
-    public static void printArr(int a[]){
-        for(int i=0;i<a.length;i++){
-            System.out.print(a[i]+" ");
-            
+
+    public static void selectionSort(int a[]){
+        int n=a.length;
+        int min=Integer.MAX_VALUE;
+
+        for(int i=0;i<n-1;i++){
+            int minPos=i;
+            for(int j=i;j<n;j++){
+
+                if(a[minPos]>a[j]){
+                    minPos=j;
+                }
+
+            }
+
+            swapArr(a,minPos,i);
         }
-        System.out.println("");
     }
+
+   
 
     public static void main(String args[]){
 
         int arr[]={5,2,3,1,4};
         int a2[]={1,2,3,4,5};
-        bubbleSort(arr);
+       /*  bubbleSort(arr);
         printArr(arr);
-        bubbleSort(a2);
+        bubbleSort(a2); */
+        selectionSort(arr);
+        printArr(arr);
 
 
     }
