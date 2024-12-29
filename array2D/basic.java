436c3cae015ad92a0f3927942df137eb9cf3a366
Atharva <athrudev@gmail.com>
2024-12-29 11:18:23 +0530

    started 2D array with some basic operation

---
+++ array2D/basic.java
@@ -0,0 +1,71 @@
+package array2D;
+import java.util.*;
+
+public class basic {
+
+    public static boolean search(int a[][],int key){
+
+        int n=a.length,m=a[0].length;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(a[i][j]==key){
+                    System.out.println(key+" found at cell: ("+i+","+j+")");
+                    
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    public static void displayArray(int a[][]){
+        int n=a.length;
+        int m=a[0].length;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.print(a[i][j]+" ");
+                
+            }
+            System.out.println();
+            
+        }
+    }
+
+    public static void main(String args[]){
+        int matric[][]=new int[3][3];
+        int n=matric.length; //row
+        int m=matric[0].length; //column
+
+        //System.out.println("Enter matrix element (9)");
+        Scanner sc=new Scanner(System.in);
+        int val=1;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                matric[i][j]=val;
+                val++;
+            }
+        }
+
+        System.out.println("matrix element's are:\n");
+        displayArray(matric);
+
+        System.out.println("\n");
+
+        search(matric, 5);
+        search(matric, 7);
+        search(matric, 9);
+        search(matric, 1);
+        
+        
+        //search(matric,9);
+        sc.close();
+
+        //Arrays.sort(matric);
+
+    }
+    
+}
