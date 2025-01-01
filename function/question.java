b909deb9d7bdab674811ee7064fbf86ea9384acc
Atharva <athrudev@gmail.com>
2025-01-01 10:08:13 +0530

    solved the prime number problem

--- function/question.java
+++ function/question.java
@@ -23,17 +23,34 @@ public class question {
         return BC;
     }
 
+
+
+    public static boolean isPrime(int n){
+
+        if(n<=1){
+            return false;
+        }
+
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         int n,r;
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the n: ");
         n=sc.nextInt();
-        System.out.print("Enter the r: ");
+        /*System.out.print("Enter the r: ");
         r=sc.nextInt();
 
         System.out.println("Factorial of "+n+" = "+fact(n));
 
-        System.out.println("Binomial coeffiecient: "+bicoe(n,r));
+        System.out.println("Binomial coeffiecient: "+bicoe(n,r));*/
+        System.out.println(n+" is Prime: "+isPrime(n));
         
         
         
