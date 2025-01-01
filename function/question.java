db7dfc85484fa95c7974d3645faf734d6d3c9771
Atharva <athrudev@gmail.com>
2025-01-01 09:58:46 +0530

    solved the binomial coefficient problem

--- function/question.java
+++ function/question.java
@@ -11,15 +11,30 @@ public class question {
         }
 
         return fact;
-
     }
+
+
+    public static int bicoe(int n,int r){
+        int fact_n=fact(n);
+        int fact_r=fact(r);
+        int fact_nmr=fact(n-r);
+
+        int BC=fact_n/(fact_r*fact_nmr);
+        return BC;
+    }
+
     public static void main(String[] args) {
-        int n;
+        int n,r;
         Scanner sc=new Scanner(System.in);
-        System.out.print("Enter the number: ");
+        System.out.print("Enter the n: ");
         n=sc.nextInt();
+        System.out.print("Enter the r: ");
+        r=sc.nextInt();
 
         System.out.println("Factorial of "+n+" = "+fact(n));
+
+        System.out.println("Binomial coeffiecient: "+bicoe(n,r));
+        
         
         
 
