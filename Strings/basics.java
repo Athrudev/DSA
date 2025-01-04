1d8af40db04fccd9cc21f5fb5a0430045137fc7f
Atharva <athrudev@gmail.com>
2025-01-04 16:55:51 +0530

    learn about the length() of an string class

--- Strings/basics.java
+++ Strings/basics.java
@@ -8,6 +8,11 @@ public class basics{
 
         //there is a small diffrence between this two in implimentation level
         //java string's are immutable
+
+        String name="Atharva Kamble";
+        System.out.println("length of an string is: "+name.length());
+        //in string length is an function that's why there is an parenthesis in the end
+        //in arrary, length is an property of an array that's why we don't use parenthesis
         
     }
 }
