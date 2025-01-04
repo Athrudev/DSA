2d757a908f45591f028164dee919d7599f14cb58
Atharva <athrudev@gmail.com>
2025-01-04 18:49:06 +0530

    solved the first letter capital question

--- Strings/q.java
+++ Strings/q.java
@@ -48,6 +48,30 @@ public class q {
     }
 
 
+    public static String FirstLetterCapital(String s){
+        int n=s.length();
+        StringBuilder sb=new StringBuilder("");
+
+        char c=s.charAt(0);
+        sb.append(Character.toUpperCase(c));
+
+        for(int i=1;i<n;i++){
+
+            if(s.charAt(i)==' ' && i<n-1){
+                sb.append(s.charAt(i));
+                i++;
+                
+                sb.append(Character.toUpperCase(s.charAt(i)));
+            }else{
+                sb.append(s.charAt(i));
+            }
+        }
+
+        return sb.toString();
+
+    }
+
+
     public static void main(String args[]){
        /*  String path="WNEENEENNN";
         String path2="NS";
@@ -56,8 +80,15 @@ public class q {
          */
 
 
-        String f[]={"apple","mango","banana"};
-        System.out.println("Largest is: "+largestString(f));
+        /* String f[]={"apple","mango","banana"};
+        System.out.println("Largest is: "+largestString(f)); */
+
+
+        String s="hi, i am Atharva";
+        System.out.println(s);
+        
+        System.out.println(FirstLetterCapital(s));
+        
         
     }
 }
