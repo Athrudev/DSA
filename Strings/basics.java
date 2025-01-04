801463bbc946af9c508b5d04c067ec6bea4aed38
Atharva <athrudev@gmail.com>
2025-01-04 17:25:00 +0530

    learn the optimized palindrome code

--- Strings/basics.java
+++ Strings/basics.java
@@ -12,8 +12,9 @@ public class basics{
     }
 
 
+    //this function has the time complexity of O(n)
     public static boolean isPal(String s){
-        System.out.println("Checking string: " + s);
+       // System.out.println("Checking string: " + s);
         String temp="";
         for(int i=s.length()-1;i>=0;i--){
         temp+=s.charAt(i);
@@ -31,6 +32,21 @@ public class basics{
     return false;
 }
 
+
+    public static boolean isPalindrome(String s){
+        int n=s.length();
+        for(int i=0;i<n/2;i++){
+
+            if(s.charAt(i)!=s.charAt(n-i-1)){
+                //not a palindrome condition
+                return false;
+            }
+
+        }
+
+        return true;
+
+    }
     public static void main(String[] args) {
         char c[]={'a','b','v'};
         String s ="abcd";
@@ -54,8 +70,9 @@ public class basics{
 
         printLetters(fullname);
 
-        String pal="nano";
+        String pal="noon";
         System.out.println(pal+" is palindrome: "+isPal(pal));
+        System.out.println(pal+" is palindrome (optimized code): "+isPalindrome(pal));
         
 
         
