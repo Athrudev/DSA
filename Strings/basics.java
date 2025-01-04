9519657d72cf5e01968dc2f6d12fa4ec2178c348
Atharva <athrudev@gmail.com>
2025-01-04 16:48:54 +0530

    started the string section with some basics

---
+++ Strings/basics.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class basics{
+    public static void main(String[] args) {
+        char c[]={'a','b','v'};
+        String s ="abcd";
+        String s2=new String("xyz");
+
+        //there is a small diffrence between this two in implimentation level
+        //java string's are immutable
+        
+    }
+}
