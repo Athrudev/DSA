eb1f163f38f5b50afdcc6597025d7b103ecc2163
Atharva <athrudev@gmail.com>
2025-01-12 12:06:23 +0530

    perform the ith bit operation

--- bit manipulation/q.java
+++ bit manipulation/q.java
@@ -10,10 +10,23 @@ public class q {
 
     }
 
+
+    public static int getIthBit(int n,int i){
+        int bitMask=(1<<i);
+        if((n&bitMask)==0){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+
     public static void main(String[] args) {
         OddorEven(3);
         OddorEven(4);
         OddorEven(480);
         OddorEven(109);
+
+        System.out.println(getIthBit(10, 3));
+        
     }
 }
