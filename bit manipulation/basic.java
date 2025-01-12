659854513772fb4e950eec844980c15b4a2ad927
Atharva <athrudev@gmail.com>
2025-01-12 11:17:03 +0530

    modified the code

--- bit manipulation/basic.java
+++ bit manipulation/basic.java
@@ -4,7 +4,11 @@ public class basic{
     public static void main(String args[]){
         int a,b;
         Scanner sc=new Scanner(System.in);
+
+        System.out.print("\na: ");        
         a=sc.nextInt();
+
+        System.out.print("\nb: ");
         b=sc.nextInt();
 
         System.out.println(a+" & "+b+" :"+(a&b)); //this is AND operation
